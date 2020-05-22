d86a91dcba931d8e28d9042a8a7aab4b50016cfb
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-05-22 19:47:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Um triângulo retângulo possui uma báse de 3 e altura 5, qual é o valor de sua área?");
+            double B=3;
+            double A=5;
+            double resultado=B * A;
+            double resultado2=resultado / 2;
+            Console.WriteLine($"{resultado2}");
+        }
+    }
+}
