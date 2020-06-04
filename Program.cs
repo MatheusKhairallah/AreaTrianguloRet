791494c186a87e50d540830d227c7e8001e0ba74
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:22:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva o valor da báse e a altura de um triangulo que então será calculado a área do mesmo");
+            Console.Write("Báse:");
+            double B=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Altura:");
+            double A=Convert.ToDouble(Console.ReadLine());
+            double resultado=B * A;
+            double resultado2=resultado / 2;
+            Console.Clear();
+            Console.WriteLine($"A área deste triangulo é= {resultado2}");
+        }
+    }
+}
